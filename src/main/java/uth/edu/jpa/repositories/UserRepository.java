e0ef0affb854aafec0bbceb6f10268e81af37ffe
Nguyen Anh Huy <soicaca77@gmail.com>
2025-03-16 20:32:49 +0700

    User - Model, Controller, Repository, Service

--- src/main/java/uth/edu/jpa/repositories/UserRepository.java
+++ src/main/java/uth/edu/jpa/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package uth.edu.jpa.repositories;
-import org.springframework.stereotype.Repository;
-import uth.edu.jpa.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-@Repository
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uth.edu.jpa.models.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
