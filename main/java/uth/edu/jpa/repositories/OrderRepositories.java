05abed5c9f7e2c3ed50fd226ef9ab76e53675f78
Nguyen Anh Huy <soicaca77@gmail.com>
2025-03-24 12:31:26 +0700

    SUA LOI PUSH

--- main/java/uth/edu/jpa/repositories/OrderRepositories.java
+++
@@ -1,9 +0,0 @@
-package uth.edu.jpa.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    // Các phương thức truy vấn tùy chỉnh (nếu cần)
-}
