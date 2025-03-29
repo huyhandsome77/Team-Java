c96e8aea972fffd967c22300cc655b0f4344142a
ThanhDev <thanhpqwetyu@gmail.com>
2025-03-29 20:13:26 +0700

    sửa template và controlller-menu-footer-navbar( Quản lý sân)

--- src/main/java/uth/edu/jpa/controllers/QLSController.java
+++ src/main/java/uth/edu/jpa/controllers/QLSController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class QlsController {
+public class QLSController {
 
     @GetMapping("/QuanLySan")
     public String QuanLySanPage() {
