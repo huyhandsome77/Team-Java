cdf5f4dc2f441e5dc6b73cea2ed6bd08e38fe55e
ThanhDev <thanhpqwetyu@gmail.com>
2025-03-31 03:42:44 +0700

    update pages Chart, Booking, QuanLySan_Dashboard

--- src/main/java/uth/edu/jpa/controllers/QLSController.java
+++ src/main/java/uth/edu/jpa/controllers/QLSController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class QLSController {
 
