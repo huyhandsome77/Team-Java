9434f32ab7048fea01180788eb7e80275f774baf
HGiaHop <giahop09102005@gmail.com>
2025-03-31 16:05:50 +0700

    oder

---
+++ src/main/java/uth/edu/jpa/repositories/OrderRepositories.java
@@ -0,0 +1,9 @@
+package uth.edu.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
